96407d2cd14dd5f2872f540ba5a7b23d41564954
Mwaki Magotswi <magotswi@gmail.com>
2015-03-21 19:53:46 -0400

    Adding views and using them

--- concert_scrapper.rb
+++ concert_scrapper.rb
@@ -1,15 +1,15 @@
+require 'sinatra'
 require 'nokogiri'
 require 'open-uri'
 
-url = "http://www.930.com/concerts/"
+get '/' do
+  url = "http://www.930.com/concerts/"
+  page = Nokogiri::HTML(open(url))
+  @concerts = page.css('.list-view-item')
+  
+  erb :show_listing
+end
 
-data = Nokogiri::HTML(open(url))
-
-data.css('.list-view-item').each do |concert|
-  puts concert.at_css('.summary').text
-  puts concert.at_css('.supports').text unless concert.at_css('.supports').nil?
-  puts concert.at_css('.dates').text
-  puts concert.at_css('.doors').text
-  puts concert.at_css('.custom').nil? ? "SOLD OUT" : concert.at_css('.price-range').text.strip
-  puts ""
+get '/interesting' do
+  erb :interesting
 end
