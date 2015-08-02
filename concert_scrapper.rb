63051c190ee8b09e4d0d46c92bda5154cdad1990
Mwaki Magotswi <magotswi@gmail.com>
2015-08-02 15:48:07 -0400

    trying a different approach

--- concert_scrapper.rb
+++ concert_scrapper.rb
@@ -3,11 +3,19 @@ require 'nokogiri'
 require 'open-uri'
 
 get '/' do
+  "Will it blend?"
+end
+
+get '/club930' do
   url = "http://www.930.com/concerts/"
   page = Nokogiri::HTML(open(url))
   @concerts = page.css('.list-view-item')
   
   erb :show_listing
+end
+
+get '/thewilbur'
+"Work in Progress"
 end
 
 get '/interesting' do
